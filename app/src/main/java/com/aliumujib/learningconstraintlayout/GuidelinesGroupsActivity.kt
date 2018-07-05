4048598a4a43c610d468f0b1d2f9740208d945e1
aliumujib <aliuabdulmujib@yahoo.com>
2018-07-05 09:19:11 +0100

    Completed groups and guidlines module

---
+++ app/src/main/java/com/aliumujib/learningconstraintlayout/GuidelinesGroupsActivity.kt
@@ -0,0 +1,19 @@
+package com.aliumujib.learningconstraintlayout
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_guidelines_groups.*
+
+class GuidelinesGroupsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guidelines_groups)
+        setSupportActionBar(toolbar)
+
+
+    }
+
+}
